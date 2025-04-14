0917212347a7bea1b48231214e5f2d549a1ae5f5
Lauren Peppard <peppard_lauren@yahoo.com>
2025-04-14 08:48:23 -0500

    customRender

--- src/mocks/customRender.tsx
+++ src/mocks/customRender.tsx
@@ -1,13 +1,18 @@
-import { render, act } from '@testing-library/react';
+import { render, act, RenderResult } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import QueryClientProvider from '../components/shared/QueryClientProvider';
 import { axe, toHaveNoViolations } from 'jest-axe';
 import { HelmetProvider } from 'react-helmet-async';
+import { ReactElement } from 'react';
 
 expect.extend(toHaveNoViolations);
 
-export const customRender = component => {
-	const { container } = render(
+interface CustomRenderResult extends RenderResult {
+  axeTest: () => Promise<void>;
+}
+
+export const customRender = (component: ReactElement): CustomRenderResult => {
+	const { container, ...rest } = render(
 		<MemoryRouter initialEntries={['/']}>
 			<QueryClientProvider>
 				<HelmetProvider>{component}</HelmetProvider>
@@ -16,6 +21,8 @@ export const customRender = component => {
 	);
 
 	return {
+		...rest,
+		container,
 		// Call this to make sure that there are no A11y violations.
 		axeTest: async () =>
 			act(async () => expect(await axe(container)).toHaveNoViolations()),
