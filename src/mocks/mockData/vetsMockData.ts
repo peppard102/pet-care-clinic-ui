74f48d2a485bf490a1ee41d0debf96495af8cd50
Lauren Peppard <peppard_lauren@yahoo.com>
2025-04-16 21:47:49 -0500

    ts error

--- src/mocks/mockData/vetsMockData.ts
+++ src/mocks/mockData/vetsMockData.ts
@@ -1,13 +1,13 @@
 import { Vet } from "../../types";
 
 export const vetsMockData: Vet[] = [
-	{ id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-	{ id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-	{ id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-	{ id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-	{ id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: 150 },
-	{ id: 6, lastName: 'Melisandre', firstName: 'Chester', age: 150 },
-	{ id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-	{ id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-	{ id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+	{ id: 1, lastName: 'Snow', firstName: 'Jon', age: 35, specialization: 'Dermatology' },
+	{ id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42, specialization: 'Cardiology' },
+	{ id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45, specialization: 'Oncology' },
+	{ id: 4, lastName: 'Stark', firstName: 'Arya', age: 16, specialization: 'Pediatrics' },
+	{ id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: 150, specialization: 'Surgery' },
+	{ id: 6, lastName: 'Melisandre', firstName: 'Chester', age: 150, specialization: 'Radiology' },
+	{ id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44, specialization: 'Dermatology' },
+	{ id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36, specialization: 'Cardiology' },
+	{ id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65, specialization: 'Oncology' },
 ];
