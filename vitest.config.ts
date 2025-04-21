46cd0404796e885c001ef3ed9a0d5d214456ccfa
peppard102 <37004746+peppard102@users.noreply.github.com>
2025-04-21 17:43:37 -0500

    build: Add docker build to pipeline (#6)
    * docker step
    
    * Copy over dockerfile
    
    * nginx
    
    * fix coverage exclusion
    
    * fix folder name

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from 'vitest/config';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
@@ -9,10 +9,10 @@ export default defineConfig({
       include: ['src/**/*.ts', 'src/**/*.tsx'],
       exclude: [
         'src/mocks/browser.ts',
-        'src/utils/testHelperFunctions.js',
+        'src/utils/testHelperFunctions.ts',
         'src/types/**',
         'src/vite-env.d.ts',
-        'src/main.tsx'
+        'src/main.tsx',
       ],
     },
   },
