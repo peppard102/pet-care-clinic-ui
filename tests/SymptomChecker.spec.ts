e1c06b39c9714e22209e5cc5588c857395986dbf
Lauren Peppard <peppard_lauren@yahoo.com>
2025-05-23 16:44:54 -0500

    base URL

--- tests/SymptomChecker.spec.ts
+++ tests/SymptomChecker.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('Has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('/');
 
   // Validate page title
   await expect(page).toHaveTitle(/Pet Care Clinic/);
 });
 
 test('Has heading', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('/');
 
   // Validate heading visibility
   await expect(
