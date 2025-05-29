9f5dbe45f03c91ffdf5a277f9bffae252b97415e
Lauren Peppard <peppard_lauren@yahoo.com>
2025-05-29 18:38:04 -0500

    fix data

--- src/components/vets/VetsPage.tsx
+++ src/components/vets/VetsPage.tsx
@@ -9,90 +9,89 @@ import { useState } from 'react';
 import { Vet } from '../../types';
 
 const style = {
-	position: 'absolute',
-	top: '50%',
-	left: '50%',
-	transform: 'translate(-50%, -50%)',
-	width: 400,
-	bgcolor: 'background.paper',
-	border: '1px solid #000',
-	boxShadow: 24,
-	p: 4,
-	borderRadius: '8px',
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '1px solid #000',
+  boxShadow: 24,
+  p: 4,
+  borderRadius: '8px',
 } as const;
 
 const columns: GridColDef[] = [
-	{ field: 'id', headerName: 'ID', width: 90 },
-	{
-		field: 'firstName',
-		headerName: 'First name',
-		width: 150,
-		editable: true,
-	},
-	{
-		field: 'lastName',
-		headerName: 'Last name',
-		width: 150,
-		editable: true,
-	},
-	{
-		field: 'age',
-		headerName: 'Age',
-		type: 'number',
-		width: 110,
-		editable: true,
-	},
-	{
-		field: 'fullName',
-		headerName: 'Full name',
-		sortable: false,
-		width: 160,
-	},
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'specialty',
+    headerName: 'Specialty',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    sortable: false,
+    width: 160,
+  },
 ];
 
 export default function VetsPage(): React.ReactElement {
-	const vets: Vet[] = useVets();
-	const [open, setOpen] = useState<boolean>(false);
-	const handleOpen = (): void => setOpen(true);
-	const handleClose = (): void => setOpen(false);
+  const vets: Vet[] = useVets();
+  const [open, setOpen] = useState<boolean>(false);
+  const handleOpen = (): void => setOpen(true);
+  const handleClose = (): void => setOpen(false);
 
-	return (
-		<PageContainer title="Vets">
-			<Box sx={{ height: 400, width: '100%' }}>
-				<DataGrid
-					rows={vets}
-					columns={columns}
-					initialState={{
-						pagination: {
-							paginationModel: {
-								pageSize: 5,
-							},
-						},
-					}}
-					pageSizeOptions={[5]}
-					checkboxSelection
-					disableRowSelectionOnClick
-				/>
-			</Box>
+  return (
+    <PageContainer title="Vets">
+      <Box sx={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={vets}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+      </Box>
 
-			<Button variant="contained" onClick={handleOpen}>
-				Open modal
-			</Button>
-			<Modal
-				open={open}
-				onClose={handleClose}
-				aria-labelledby="modal-modal-title"
-				aria-describedby="modal-modal-description"
-			>
-				<Box sx={style}>
-					<Typography id="modal-modal-title" variant="h6" component="h2">
-						Text in a modal
-					</Typography>
-					<Typography id="modal-modal-description" sx={{ mt: 2 }}>
-						Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-					</Typography>
-				</Box>
-			</Modal>
-		</PageContainer>
-	);
+      <Button variant="contained" onClick={handleOpen}>
+        Open modal
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </PageContainer>
+  );
 }
--- src/mocks/mockData/vetsMockData.ts
+++ src/mocks/mockData/vetsMockData.ts
@@ -1,13 +1,43 @@
-import { Vet } from "../../types";
+import { Vet } from '../../types';
 
 export const vetsMockData: Vet[] = [
-	{ id: 1, lastName: 'Snow', firstName: 'Jon', age: 35, specialization: 'Dermatology' },
-	{ id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42, specialization: 'Cardiology' },
-	{ id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45, specialization: 'Oncology' },
-	{ id: 4, lastName: 'Stark', firstName: 'Arya', age: 16, specialization: 'Pediatrics' },
-	{ id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: 150, specialization: 'Surgery' },
-	{ id: 6, lastName: 'Melisandre', firstName: 'Chester', age: 150, specialization: 'Radiology' },
-	{ id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44, specialization: 'Dermatology' },
-	{ id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36, specialization: 'Cardiology' },
-	{ id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65, specialization: 'Oncology' },
+  { id: 1, lastName: 'Snow', firstName: 'Jon', specialty: 'Dermatology' },
+  {
+    id: 2,
+    lastName: 'Lannister',
+    firstName: 'Cersei',
+    specialty: 'Cardiology',
+  },
+  {
+    id: 3,
+    lastName: 'Lannister',
+    firstName: 'Jaime',
+    specialty: 'Oncology',
+  },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', specialty: 'Pediatrics' },
+  {
+    id: 5,
+    lastName: 'Targaryen',
+    firstName: 'Daenerys',
+    specialty: 'Surgery',
+  },
+  {
+    id: 6,
+    lastName: 'Melisandre',
+    firstName: 'Chester',
+    specialty: 'Radiology',
+  },
+  {
+    id: 7,
+    lastName: 'Clifford',
+    firstName: 'Ferrara',
+    specialty: 'Dermatology',
+  },
+  {
+    id: 8,
+    lastName: 'Frances',
+    firstName: 'Rossini',
+    specialty: 'Cardiology',
+  },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', specialty: 'Oncology' },
 ];
